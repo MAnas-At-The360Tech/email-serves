d23c401b74b3102212ef6f1c5fa09532d69c9ef2
MAnasThe360Tech <118936524+MAnasThe360Tech@users.noreply.github.com>
2022-11-29 18:43:31 +0000

    complate

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var nodemailer = require('nodemailer');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'm.anas.the360tech@gmail.com',
+      pass: 'gugoqdbonfvpvesy'
+    }
+  });
+
+  var mailOptions = {
+    from: 'm.anas.the360tech@gmail.com',
+    to: 'm.anaslatif.com@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.send('error')
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Ok')
+    }
+  });
 })
 
 app.listen(port, () => {
